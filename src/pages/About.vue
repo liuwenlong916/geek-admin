eaad940c59fdc8615affb0595890f7f40157b047
liuwenlong916 <1141657011@qq.com>
2022-10-20 11:06:17 +0800

    vite+router+vuex+eslint+preitter

---
+++ src/pages/About.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>关于页面 {{ a }}</div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      a: '1'
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped></style>
